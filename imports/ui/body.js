6e180194af9e5bac5bfeea8f34ff77e5268ee1bc
Fady <fadysamirsadek@live.com>
2017-03-11 18:17:43 +0200

    Created article creation form

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -7,4 +7,23 @@ Template.articles.helpers({
   articles(){
     return Articles.find();
   },
+  
+});
+Template.articleCreator.events({
+    'submit  .article-creator'(event){
+      event.preventDefault();
+      const target = event.target ;
+      const title = target.title.value;
+      const text = target.text.value;
+      Articles.insert({
+        title,
+        text,
+        createdBy : Meteor.userId(),
+        createdAt : new Date(),
+        
+      });
+      target.text.value= "";
+      target.title.value="";
+      
+    }
 });
--- server/main.js
+++ server/main.js
@@ -3,12 +3,6 @@ import {Articles} from '../imports/api/articles.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
-  if (Articles.find().count() < 1) {
-   [{ title: 'Back to Work', url: 'http://5by5.tv/b2w' },
-    { title: 'Rodering on the Line', url: 'http://www.merlinmann.com/roderick' },
-    { title: 'Here\'s the Thing', url: 'http://www.wnyc.org/shows/heresthething' },
-    { title: 'Evening Dhamma Talks', url: 'http://www.dhammatalks.org/mp3_index.html' }]
-   .forEach((article) => Articles.insert(article));
- }
+  
   
 });
