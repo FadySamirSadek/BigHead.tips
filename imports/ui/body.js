5edca295af272c9937d0fc2ce1ac59e44647d650
Fady <fadysamirsadek@live.com>
2017-03-19 01:41:09 +0200

    created the markdown editor

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,8 +1,21 @@
 import './body.html';
 import {Articles} from '../api/articles.js';
 import { Template } from 'meteor/templating';
+import { Session } from 'meteor/session';
 
-
+Template.editor.helpers({
+  text(){
+    return Session.get("content");
+  },
+});
+Template.editor.events({
+  'keyup .content'(event){
+    event.preventDefault();
+    const target = event.target ;
+    const content = target.value;
+    Session.set("content", content);
+  }
+});
 Template.articles.helpers({
   articles(){
     return Articles.find();
