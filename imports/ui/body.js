94380d085d42afcc04e6490f7380a3921cf6503b
Fady <fadysamirsadek@live.com>
2017-03-23 17:59:12 +0200

    styled the conten creation page

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,7 +2,6 @@ import './body.html';
 import {Articles} from '../api/articles.js';
 import { Template } from 'meteor/templating';
 import { Session } from 'meteor/session';
-
 Template.editor.helpers({
   text(){
     return Session.get("content");
